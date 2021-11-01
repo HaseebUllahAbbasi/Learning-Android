0c529e02c608f3716ea2aa653fa4532aa250f1c8
Haseeb U <haseebabbasi00@gmail.com>
2021-11-01 22:29:48 +0500

    api fetching done

--- apiDot/app/src/main/java/com/dot/apidot/MainActivity.java
+++ apiDot/app/src/main/java/com/dot/apidot/MainActivity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 import static android.content.ContentValues.TAG;
+import static android.widget.Toast.LENGTH_LONG;
+import static com.android.volley.Request.Method.GET;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +31,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hitApi();
+//        jsonApi();
+        jsonApiArray();
     }
 
-    public synchronized void  hitApi()
-    {
+    RequestQueue requestQueue;
+
+    private void jsonApi() {
+        TextView textView = findViewById(R.id.textView_1);
+        requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(GET, "https://jsonplaceholder.typicode.com/posts/1", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "onResponse: " + response);
+                Toast.makeText(getApplicationContext(), response.toString(), LENGTH_LONG).show();
+                textView.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+    private void jsonApiArray() {
+        TextView textView = findViewById(R.id.textView_1);
+        requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(GET, "https://jsonplaceholder.typicode.com/posts", null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response)
+            {
+                Log.d(TAG, "onResponse: "+response.toString());
+                textView.setText(response.toString());
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
+
+    public void hitApi() {
         TextView textView = (TextView) findViewById(R.id.text);
         final String[] data = {""};
 // ...
@@ -34,19 +86,19 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://jsonplaceholder.typicode.com/posts/1";
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener() {
+        StringRequest stringRequest = new StringRequest(GET, url, new Response.Listener() {
             @Override
             public synchronized void onResponse(Object response) {
-                Log.d(TAG, "onResponse: " + response.toString() ) ;
+                Log.d(TAG, "onResponse: " + response.toString());
                 textView.setText(response.toString());
-                data[0] =response.toString();
+                data[0] = response.toString();
             }
         }, new Response.ErrorListener() {
             @Override
             public synchronized void onErrorResponse(VolleyError error) {
             }
         });
-        Log.d(TAG, "hitApi: "+ data[0]);
+        Log.d(TAG, "hitApi: " + data[0].toString());
 
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
@@ -60,9 +112,8 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://www.google.com";
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, (Response.Listener) response ->
+        StringRequest stringRequest = new StringRequest(GET, url, (Response.Listener) response ->
         {
-
 
         }, error -> {
 
