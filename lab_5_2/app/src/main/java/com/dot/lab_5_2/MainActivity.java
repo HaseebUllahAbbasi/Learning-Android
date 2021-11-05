09a5eed2c7328d7050c395cb4bed214be276d339
Haseeb U <haseebabbasi00@gmail.com>
2021-11-06 01:26:40 +0500

    lab 5 _2

---
+++ lab_5_2/app/src/main/java/com/dot/lab_5_2/MainActivity.java
@@ -0,0 +1,12 @@
+package com.dot.lab_5_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
